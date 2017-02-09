9b2a1d5fd649640e72f44976f09c9ba03bea81ee
Philippe Kalitine <philippe.kalitine@gmail.com>
2017-02-09 10:25:04 +0100

    feat(): export AbstractMessage

--- lib-src/index.ts
+++ lib-src/index.ts
@@ -1,4 +1,4 @@
 export { Collaborator, CollaboratorsService } from './collaborators/'
 export { DocService } from './doc/'
-export { BroadcastMessage, JoinEvent, NetworkMessage, SendRandomlyMessage, SendToMessage } from './network/'
+export { BroadcastMessage, JoinEvent, NetworkMessage, SendRandomlyMessage, SendToMessage, AbstractMessage } from './network/'
 export { MuteCore } from './MuteCore'
