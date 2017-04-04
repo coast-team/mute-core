fad24a7d6b5784e775c2f9001b835f074d76aa0b
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-04-04 10:43:55 +0200

    fix(sync): Remove buffers mechanisms
    These mechanisms were complexifying the code.
    They also proved themselves to be an additional source of errors when the messages of the synchronization process were lost. In that case, nobody was ever 'synced'

--- lib-src/sync/SyncService.ts
+++ lib-src/sync/SyncService.ts
@@ -11,22 +11,17 @@ import { JoinEvent } from '../network/'
 export class SyncService {
 
   private id: number = -1
-  private isSync: boolean = false
   private clock: number = 0
   private vector: Map<number, number> = new Map()
   private richLogootSOps: RichLogootSOperation[] = []
 
   private isReadySubject: Subject<void>
-  private isSyncSubject: Subject<void>
   private localRichLogootSOperationSubject: Subject<RichLogootSOperation>
   private querySyncSubject: Subject<Map<number, number>>
   private remoteLogootSOperationSubject: Subject<(LogootSAdd | LogootSDel)[]>
-  private remoteQuerySyncSubject: Subject<Map<number, number>>
-  private remoteRichLogootSOperationSubject: Subject<RichLogootSOperation>
   private replySyncSubject: Subject<ReplySyncEvent>
   private stateSubject: Subject<State>
 
-  private isSyncSubscriptions: Subscription[]
   private localLogootSOperationSubscription: Subscription
   private remoteQuerySyncSubscription: Subscription
   private remoteReplySyncSubscription: Subscription
@@ -37,21 +32,12 @@ export class SyncService {
   constructor (id: number) {
     this.id = id
     this.isReadySubject = new Subject<void>()
-    this.isSyncSubject = new Subject<void>()
     this.localRichLogootSOperationSubject = new Subject()
     this.querySyncSubject = new Subject()
     this.remoteLogootSOperationSubject = new Subject()
-    this.remoteQuerySyncSubject = new Subject()
-    this.remoteRichLogootSOperationSubject = new Subject()
     this.replySyncSubject = new Subject()
     this.stateSubject = new Subject()
 
-    this.isSyncSubscriptions = []
-
-    const isSyncSubscription = this.isSyncSubject.subscribe(() => {
-      this.isSync = true
-    })
-    this.isSyncSubscriptions.push(isSyncSubscription)
   }
 
   get onLocalRichLogootSOperation (): Observable<RichLogootSOperation> {
@@ -92,26 +78,8 @@ export class SyncService {
   }
 
   set remoteQuerySyncSource (source: Observable<Map<number, number>>) {
-    let buffer: Array<Map<number, number>> = []
-
     this.remoteQuerySyncSubscription = source.subscribe((vector: Map<number, number>) => {
-      if (this.isSync) {
-        this.remoteQuerySyncSubject.next(vector)
-      } else {
-        buffer.push(vector)
-      }
-    })
-
-    const isSyncSubscription = this.isSyncSubject.subscribe(() => {
-      buffer.forEach((vector: Map<number, number>) => {
-        this.remoteQuerySyncSubject.next(vector)
-      })
-      buffer = []
-    })
-    this.isSyncSubscriptions.push(isSyncSubscription)
-
-    this.remoteQuerySyncSubject.subscribe((vector: Map<number, number>) => {
-      const missingRichLogootSOps: RichLogootSOperation[] = this.richLogootSOps.filter((richLogootSOperation: RichLogootSOperation) => {
+        const missingRichLogootSOps: RichLogootSOperation[] = this.richLogootSOps.filter((richLogootSOperation: RichLogootSOperation) => {
         const id: number = richLogootSOperation.id
         const clock: number = richLogootSOperation.clock
         const v = vector.get(id)
@@ -156,34 +124,14 @@ export class SyncService {
             this.localRichLogootSOperationSubject.next(richLogootSOp)
           })
       })
-
-      this.isSyncSubject.next()
     })
   }
 
   set remoteRichLogootSOperationSource (source: Observable<RichLogootSOperation>) {
-    let buffer: Array<RichLogootSOperation> = []
-
     this.remoteRichLogootSOperationSubscription = source.subscribe((richLogootSOp: RichLogootSOperation) => {
-      if (this.isSync) {
-        this.remoteRichLogootSOperationSubject.next(richLogootSOp)
-      } else {
-        buffer.push(richLogootSOp)
-      }
-    })
-
-    this.remoteRichLogootSOperationSubject.subscribe((richLogootSOp: RichLogootSOperation) => {
       this.applyRichLogootSOperations([richLogootSOp])
       this.stateSubject.next(this.state)
     })
-
-    const isSyncSubscription = this.isSyncSubject.subscribe(() => {
-      buffer.forEach((richLogootSOp: RichLogootSOperation) => {
-        this.remoteRichLogootSOperationSubject.next(richLogootSOp)
-      })
-      buffer = []
-    })
-    this.isSyncSubscriptions.push(isSyncSubscription)
   }
 
   private set storedStateSource (source: Observable<State>) {
@@ -220,14 +168,9 @@ export class SyncService {
     this.localRichLogootSOperationSubject.complete()
     this.querySyncSubject.complete()
     this.remoteLogootSOperationSubject.complete()
-    this.remoteQuerySyncSubject.complete()
-    this.remoteRichLogootSOperationSubject.complete()
     this.replySyncSubject.complete()
     this.stateSubject.complete()
 
-    this.isSyncSubscriptions.forEach((subscription: Subscription) => {
-      subscription.unsubscribe()
-    })
     this.localLogootSOperationSubscription.unsubscribe()
     this.remoteQuerySyncSubscription.unsubscribe()
     this.remoteReplySyncSubscription.unsubscribe()
@@ -248,36 +191,18 @@ export class SyncService {
       })
 
     if (newRichLogootSOps.length > 0) {
-      const minClockById = newRichLogootSOps.reduce((vector: Map<number, number>, richLogootSOp: RichLogootSOperation) => {
-        // Keep only the oldest operation for each id
-        const v = vector.get(richLogootSOp.id)
-        if (v === undefined || v > richLogootSOp.clock) {
-          vector.set(richLogootSOp.id, richLogootSOp.clock)
-        }
-        return vector
-      }, new Map())
-
-      let isNotAppliable = false
-      minClockById.forEach((clock: number, id: number) => {
-        if (!this.isAppliable(id, clock)) {
-          isNotAppliable = true
-        }
-      })
-
-      if (isNotAppliable) {
-        this.isSync = false
-        // Trigger a QuerySync message
-        this.querySyncSubject.next(this.vector)
-      } else {
-        const logootSOperations: (LogootSAdd | LogootSDel)[] = []
-        newRichLogootSOps
-          .forEach((richLogootSOp) => {
+      const logootSOperations: (LogootSAdd | LogootSDel)[] = []
+      newRichLogootSOps
+        .forEach((richLogootSOp) => {
+          const id: number = richLogootSOp.id
+          const clock: number = richLogootSOp.clock
+          if (this.isAppliable(id, clock)) {
             this.updateState(richLogootSOp)
             logootSOperations.push(richLogootSOp.logootSOp)
-          })
+          }
+        })
 
-        this.remoteLogootSOperationSubject.next(logootSOperations)
-      }
+      this.remoteLogootSOperationSubject.next(logootSOperations)
     }
   }
 
