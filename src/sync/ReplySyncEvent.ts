02f89cdb0b78b5ca780c2f2be818e924acc529a7
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-08-11 10:54:29 +0200

    feat(ReplySyncEvent): Add equals()

--- src/sync/ReplySyncEvent.ts
+++ src/sync/ReplySyncEvent.ts
@@ -10,4 +10,17 @@ export class ReplySyncEvent {
     this.richLogootSOps = richLogootSOps
     this.intervals = intervals
   }
+
+  equals (aOther: ReplySyncEvent) {
+    return this.richLogootSOps.length === aOther.richLogootSOps.length &&
+      this.intervals.length === aOther.intervals.length &&
+      this.richLogootSOps.every((richLogootSOp: RichLogootSOperation, index: number): boolean => {
+        const otherRichLogootSOp = aOther.richLogootSOps[index]
+        return richLogootSOp.equals(otherRichLogootSOp)
+      }) &&
+      this.intervals.every((interval: Interval, index: number): boolean => {
+        const otherInterval = aOther.intervals[index]
+        return interval.equals(otherInterval)
+      })
+  }
 }
