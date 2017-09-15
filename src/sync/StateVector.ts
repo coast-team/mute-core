7688feef4d59fb5fcc9416daa9f36617431d56a0
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-09-15 13:10:53 +0200

    feat: Add StateVector

---
+++ src/sync/StateVector.ts
@@ -0,0 +1,69 @@
+/**
+ * Keep track of the messages delivered by peers
+ * Allow to maintain the causal delivery per peer
+ */
+export class StateVector {
+
+    private vector: Map<number, number>
+
+    constructor (otherMap?: Map<number, number>) {
+        this.vector = new Map(otherMap)
+    }
+
+    get (id: number): number | undefined {
+        return this.vector.get(id)
+    }
+
+    /**
+     * Update the registered clock for a peer
+     * The clock must be valid for the update to be perform
+     * @param id The peer id
+     * @param clock The message clock
+     */
+    set (id: number, clock: number): void {
+        console.assert(clock >= 0, "clock must be positive")
+        console.assert(this.isDeliverable(id, clock))
+        this.vector.set(id, clock)
+    }
+
+    clear (): void {
+        this.vector.clear()
+    }
+
+    get size (): number {
+        return this.vector.size
+    }
+
+    /**
+     * Check if a message has already been delivered
+     * @param id The peer id
+     * @param clock The message clock
+     */
+    isAlreadyDelivered (id: number, clock: number): boolean {
+        const v = this.get(id)
+        return v !== undefined && v >= clock
+    }
+
+    /**
+     * Check if a message can be delivered
+     * A message can be delivered if:
+     *   - It is a new peer and the clock is equal to 0
+     *   - The clock is equal to the registered clock + 1
+     * @param id The peer id
+     * @param clock The message clock
+     */
+    isDeliverable (id: number, clock: number): boolean {
+        if (this.isAlreadyDelivered(id, clock)) {
+            return false
+        }
+        const v = this.get(id)
+        if (v === undefined) {
+            return clock === 0
+        }
+        return clock === v + 1
+    }
+
+    asMap (): Map<number, number> {
+        return new Map(this.vector)
+    }
+}
--- src/sync/index.ts
+++ src/sync/index.ts
@@ -2,6 +2,7 @@ export { Interval } from './Interval'
 export { ReplySyncEvent } from './ReplySyncEvent'
 export { RichLogootSOperation } from './RichLogootSOperation'
 export { State } from './State'
+export { StateVector } from './StateVector'
 export { SyncService } from './SyncService'
 export { SyncMessageService } from './SyncMessageService'
 
