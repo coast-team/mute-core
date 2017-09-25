a97f410be1d2601eafd0f3b671acefe4d0507f2f
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-09-25 10:49:07 +0200

    test: Move generateRichLogootSOps() to Helpers

--- test/Helpers.ts
+++ test/Helpers.ts
@@ -1,10 +1,40 @@
+import {
+    Identifier,
+    IdentifierInterval,
+    LogootSAdd,
+    LogootSDel
+} from "mute-structs"
+
 import {Disposable} from "../src/Disposable"
-import {StateVector} from "../src/sync"
+import {RichLogootSOperation, StateVector} from "../src/sync"
 
 export function disposeOf (disposable: Disposable, time: number): void {
     setTimeout(() => {
         disposable.dispose()
     }, time)
+}
+
+export function generateRichLogootSOps (): RichLogootSOperation[] {
+    const replicaNumber = 0
+    const clock = 0
+    const base = [0, 0, replicaNumber]
+
+    const id1 = new Identifier(base, 0)
+    const insertOp1: LogootSAdd = new LogootSAdd(id1, "hello")
+    const richLogootSOp1 =
+        new RichLogootSOperation(replicaNumber, clock, insertOp1)
+
+    const id2 = new Identifier(base, 5)
+    const insertOp2: LogootSAdd = new LogootSAdd(id2, " world")
+    const richLogootSOp2 =
+        new RichLogootSOperation(replicaNumber, clock + 1, insertOp2)
+
+    const idInterval1 = new IdentifierInterval(base, 3, 7)
+    const deleteOp1: LogootSDel = new LogootSDel([idInterval1])
+    const richLogootSOp3 =
+        new RichLogootSOperation(replicaNumber, clock + 2, deleteOp1)
+
+    return [richLogootSOp1, richLogootSOp2, richLogootSOp3]
 }
 
 export function generateVector (): StateVector {
--- test/SyncMessageService.test.ts
+++ test/SyncMessageService.test.ts
@@ -22,7 +22,7 @@ import {
     SyncMessageService
 } from "../src/sync"
 
-import {disposeOf, generateVector} from "./Helpers"
+import {disposeOf, generateRichLogootSOps, generateVector} from "./Helpers"
 
 function generateIntervals (): Interval[] {
     const intervals: Interval[] = []
@@ -38,29 +38,6 @@ function generateReplySync (): ReplySyncEvent {
     const intervals: Interval[] = generateIntervals()
 
     return new ReplySyncEvent(richLogootSOps, intervals)
-}
-
-function generateRichLogootSOps (): RichLogootSOperation[] {
-    const replicaNumber = 0
-    const clock = 0
-    const base = [0, 0, replicaNumber]
-
-    const id1 = new Identifier(base, 0)
-    const insertOp1: LogootSAdd = new LogootSAdd(id1, "hello")
-    const richLogootSOp1 =
-        new RichLogootSOperation(replicaNumber, clock, insertOp1)
-
-    const id2 = new Identifier(base, 5)
-    const insertOp2: LogootSAdd = new LogootSAdd(id2, " world")
-    const richLogootSOp2 =
-        new RichLogootSOperation(replicaNumber, clock + 1, insertOp2)
-
-    const idInterval1 = new IdentifierInterval(base, 3, 7)
-    const deleteOp1: LogootSDel = new LogootSDel([idInterval1])
-    const richLogootSOp3 =
-        new RichLogootSOperation(replicaNumber, clock + 2, deleteOp1)
-
-    return [richLogootSOp1, richLogootSOp2, richLogootSOp3]
 }
 
 test("richLogootSOperations-correct-send-and-delivery", (t: TestContext) => {
