952f2a373b8d528d1bb973fefde9c246bc112960
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-09-25 11:07:27 +0200

    test(SyncService): Add test "deliver-buffered-operation-when-deliverable"

---
+++ test/SyncService.test.ts
@@ -0,0 +1,37 @@
+import test from "ava"
+import {TestContext} from "ava"
+import {LogootSOperation} from "mute-structs"
+import {Observable, Subject} from "rxjs"
+
+import {ReplySyncEvent, RichLogootSOperation, SyncService} from "../src/sync"
+import {disposeOf, generateRichLogootSOps} from "./Helpers"
+
+test("deliver-buffered-operation-when-deliverable", (t: TestContext) => {
+    const syncService = new SyncService(0)
+    disposeOf(syncService, 200)
+
+    const [firstRichLogootSOp, secondRichLogootSOp]: RichLogootSOperation[] =
+        generateRichLogootSOps()
+
+    const remoteRichLogootSOpSubject = new Subject<RichLogootSOperation>()
+    syncService.remoteRichLogootSOperationSource = remoteRichLogootSOpSubject
+
+    remoteRichLogootSOpSubject.next(secondRichLogootSOp)
+    const expectedLogootSOp: LogootSOperation =
+        secondRichLogootSOp.logootSOp
+
+    setTimeout(() => {
+        remoteRichLogootSOpSubject.next(firstRichLogootSOp)
+    }, 100)
+
+    t.plan(2)
+    return syncService.onRemoteLogootSOperation
+        .filter((logootSOps: LogootSOperation[]) => {
+            return logootSOps[0] !== firstRichLogootSOp.logootSOp
+        })
+        .map((actualLogootSOps: LogootSOperation[]) => {
+            t.is(actualLogootSOps.length, 1)
+            t.deepEqual(actualLogootSOps, [expectedLogootSOp])
+        })
+})
+
