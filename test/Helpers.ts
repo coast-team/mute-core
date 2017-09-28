0715a7fd04a0dcb6e40ce3fcca177f6bca00fca1
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2017-09-28 17:14:38 +0200

    test(Helpers): Update generateRichLogootSOps()

--- test/Helpers.ts
+++ test/Helpers.ts
@@ -1,6 +1,7 @@
 import {
     Identifier,
     IdentifierInterval,
+    IdentifierTuple,
     LogootSAdd,
     LogootSDel,
 } from 'mute-structs'
@@ -17,22 +18,23 @@ export function disposeOf (disposable: Disposable, time: number): void {
 export function generateRichLogootSOps (): RichLogootSOperation[] {
   const replicaNumber = 0
   const clock = 0
-  const base = [0, 0, replicaNumber]
+  const tuple1 = new IdentifierTuple(0, replicaNumber, clock, 0)
+  const tuple2 = IdentifierTuple.generateWithSameBase(tuple1, 5)
 
-  const id1 = new Identifier(base, 0)
+  const id1 = new Identifier([tuple1])
   const insertOp1: LogootSAdd = new LogootSAdd(id1, 'hello')
   const richLogootSOp1 =
-        new RichLogootSOperation(replicaNumber, clock, insertOp1)
+      new RichLogootSOperation(replicaNumber, clock, insertOp1)
 
-  const id2 = new Identifier(base, 5)
+  const id2 = new Identifier([tuple2])
   const insertOp2: LogootSAdd = new LogootSAdd(id2, ' world')
   const richLogootSOp2 =
-        new RichLogootSOperation(replicaNumber, clock + 1, insertOp2)
+      new RichLogootSOperation(replicaNumber, clock + 1, insertOp2)
 
-  const idInterval1 = new IdentifierInterval(base, 3, 7)
+  const id3 = Identifier.generateWithSameBase(id1, 3)
+  const idInterval1 = new IdentifierInterval(id3, 7)
   const deleteOp1: LogootSDel = new LogootSDel([idInterval1])
-  const richLogootSOp3 =
-        new RichLogootSOperation(replicaNumber, clock + 2, deleteOp1)
+  const richLogootSOp3 = new RichLogootSOperation(replicaNumber, clock + 2, deleteOp1)
 
   return [richLogootSOp1, richLogootSOp2, richLogootSOp3]
 }
