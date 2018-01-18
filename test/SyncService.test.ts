09b4eec48c649491f127b32ab675cf3018a48bad
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2018-01-18 13:39:01 +0100

    test(syncService): Remove test 'deliver-buffered-operation-when-deliverable'

--- test/SyncService.test.ts
+++ test/SyncService.test.ts
@@ -7,36 +7,6 @@ import { filter, map } from 'rxjs/operators'
 import {ReplySyncEvent, RichLogootSOperation, SyncService} from '../src/sync'
 import {disposeOf, generateSequentialRichLogootSOps} from './Helpers'
 
-test('deliver-buffered-operation-when-deliverable', (t: TestContext) => {
-  const syncService = new SyncService(0)
-  disposeOf(syncService, 200)
-
-  const [firstRichLogootSOp, secondRichLogootSOp]: RichLogootSOperation[] =
-        generateSequentialRichLogootSOps()
-
-  const remoteRichLogootSOpSubject = new Subject<RichLogootSOperation>()
-  syncService.remoteRichLogootSOperationSource = remoteRichLogootSOpSubject
-
-  remoteRichLogootSOpSubject.next(secondRichLogootSOp)
-  const expectedLogootSOp: LogootSOperation =
-        secondRichLogootSOp.logootSOp
-
-  setTimeout(() => {
-    remoteRichLogootSOpSubject.next(firstRichLogootSOp)
-  }, 100)
-
-  t.plan(2)
-  return syncService.onRemoteLogootSOperation.pipe(
-    filter((logootSOps: LogootSOperation[]) => {
-      return logootSOps[0] !== firstRichLogootSOp.logootSOp
-    }),
-    map((actualLogootSOps: LogootSOperation[]) => {
-      t.is(actualLogootSOps.length, 1)
-      t.deepEqual(actualLogootSOps, [expectedLogootSOp])
-    }),
-  )
-})
-
 test('deliver-operations-in-sequential-order', (t: TestContext) => {
   const syncService = new SyncService(0)
   disposeOf(syncService, 200)
