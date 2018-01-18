7cd3f1a2fbf18cf27b881653bee0d00bafc3c193
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2018-01-18 13:42:58 +0100

    test(syncService): Add test 'deliver-operations-in-causal-order'

--- test/SyncService.test.ts
+++ test/SyncService.test.ts
@@ -5,7 +5,7 @@ import {Observable, Subject} from 'rxjs'
 import { filter, map } from 'rxjs/operators'
 
 import {ReplySyncEvent, RichLogootSOperation, SyncService} from '../src/sync'
-import {disposeOf, generateSequentialRichLogootSOps} from './Helpers'
+import {disposeOf, generateCausalRichLogootSOps, generateSequentialRichLogootSOps} from './Helpers'
 
 test('deliver-operations-in-sequential-order', (t: TestContext) => {
   const syncService = new SyncService(0)
@@ -21,6 +21,37 @@ test('deliver-operations-in-sequential-order', (t: TestContext) => {
         .forEach((richLogootSOp: RichLogootSOperation) => {
           remoteRichLogootSOpSubject.next(richLogootSOp)
         })
+  setTimeout(() => {
+    remoteRichLogootSOpSubject.next(firstRichLogootSOp)
+  }, 100)
+
+  let counter = 0
+  t.plan(richLogootSOps.length * 2)
+  return syncService.onRemoteLogootSOperation.pipe(
+    map((actualLogootSOps: LogootSOperation[]) => {
+      const expectedLogootSOp: LogootSOperation =
+            richLogootSOps[counter].logootSOp
+      t.is(actualLogootSOps.length, 1)
+      t.deepEqual(actualLogootSOps, [expectedLogootSOp])
+      counter++
+    }),
+  )
+})
+
+test.failing('deliver-operations-in-causal-order', (t: TestContext) => {
+  const syncService = new SyncService(0)
+  disposeOf(syncService, 200)
+
+  const richLogootSOps: RichLogootSOperation[] = generateCausalRichLogootSOps()
+  const [firstRichLogootSOp, secondRichLogootSOp]: RichLogootSOperation[] = richLogootSOps
+
+  const remoteRichLogootSOpSubject = new Subject<RichLogootSOperation>()
+  syncService.remoteRichLogootSOperationSource = remoteRichLogootSOpSubject
+
+  remoteRichLogootSOpSubject.next(secondRichLogootSOp)
+  const expectedLogootSOp: LogootSOperation =
+        secondRichLogootSOp.logootSOp
+
   setTimeout(() => {
     remoteRichLogootSOpSubject.next(firstRichLogootSOp)
   }, 100)
