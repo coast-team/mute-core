bcf7977aeeaaae7370cddd22e3847329deb3cdd2
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-05-15 17:05:56 +0200

    fix(util): generateId returns correct number

--- src/util.ts
+++ src/util.ts
@@ -1,16 +1,11 @@
 export const isBrowser = typeof window !== 'undefined' ? true : false
 
 export function generateId(): number {
-  let res
   if (isBrowser) {
-    res = new Uint32Array(1)
-    window.crypto.getRandomValues(res)
+    return window.crypto.getRandomValues(new Uint32Array(1))[0]
   } else {
-    res = []
-    const bytes = require('crypto').randomBytes(4)
-    for (let i = 0; i < bytes.length; i += 4) {
-      res[res.length] = bytes.readUInt32BE(i, true)
-    }
+    return require('crypto')
+      .randomBytes(4)
+      .readUInt32BE(0, true)
   }
-  return res
 }
