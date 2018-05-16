3b505ef0b281ef5feb8541037d318c80a5c5458c
Philippe Kalitine <philippe.kalitine@gmail.com>
2018-05-16 10:42:23 +0200

    fix(util): generate int32 id instead of uint32

--- src/util.ts
+++ src/util.ts
@@ -2,10 +2,10 @@ export const isBrowser = typeof window !== 'undefined' ? true : false
 
 export function generateId(): number {
   if (isBrowser) {
-    return window.crypto.getRandomValues(new Uint32Array(1))[0]
+    return window.crypto.getRandomValues(new Int32Array(1))[0]
   } else {
     return require('crypto')
       .randomBytes(4)
-      .readUInt32BE(0, true)
+      .readInt32BE(0, true)
   }
 }
