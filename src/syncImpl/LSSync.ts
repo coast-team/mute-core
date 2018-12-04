4c620ee9608a0f16ee6e185063be8e8669494411
Cédric Enclos <cedric.enclos@loria.fr>
2018-12-04 10:40:29 +0100

    fix(lssync): fix computeDependencies method

--- src/syncImpl/LSSync.ts
+++ src/syncImpl/LSSync.ts
@@ -1,12 +1,14 @@
-import { LogootSDel, LogootSOperation } from "mute-structs";
-import { Sync } from "../core";
+import { LogootSDel, LogootSOperation } from 'mute-structs'
+import { Sync } from '../core'
 
 export class LSSync extends Sync<LogootSOperation> {
-  computeDependencies ({ lid }: LogootSDel): Map<number, number> {
+  computeDependencies(op: LogootSOperation): Map<number, number> {
     const map = new Map()
-    lid.forEach(({ idBegin: { replicaNumber } }) => {
-      map.set(replicaNumber, this.vector.get(replicaNumber))
-    })
+    if (op instanceof LogootSDel) {
+      op.lid.forEach(({ idBegin: { replicaNumber } }) => {
+        map.set(replicaNumber, this.vector.get(replicaNumber))
+      })
+    }
     return map
   }
 }
