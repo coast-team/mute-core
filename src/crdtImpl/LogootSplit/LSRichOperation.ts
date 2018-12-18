ff753cbd2596b0615233d091a0a6123664d5d592
Cédric Enclos <cedric.enclos@loria.fr>
2018-12-18 15:42:04 +0100

    fix(lsrichop): add lsrichopV1 with fromPlain for older version

--- src/crdtImpl/LogootSplit/LSRichOperation.ts
+++ src/crdtImpl/LogootSplit/LSRichOperation.ts
@@ -2,6 +2,37 @@ import { LogootSAdd, LogootSDel, LogootSOperation } from 'mute-structs'
 import { SafeAny } from 'safe-any'
 import { RichOperation } from '../../core'
 
+abstract class LSRichOperationV1 extends RichOperation<LogootSOperation> {
+  static fromPlain(o: SafeAny<LSRichOperationV1>): LSRichOperation | null {
+    if (
+      typeof o === 'object' &&
+      o !== null &&
+      typeof o.id === 'number' &&
+      Number.isInteger(o.id) &&
+      typeof o.clock === 'number' &&
+      Number.isInteger(o.clock)
+    ) {
+      const logootSAdd = LogootSAdd.fromPlain(o.logootSOp)
+      if (logootSAdd instanceof LogootSAdd) {
+        return new LSRichOperation(o.id, o.clock, logootSAdd)
+      }
+
+      const logootSDel = LogootSDel.fromPlain(o.logootSOp)
+      if (logootSDel instanceof LogootSDel) {
+        if (o.dependencies instanceof Array && o.dependencies.length > 0) {
+          return new LSRichOperation(o.id, o.clock, logootSDel, new Map(o.dependencies))
+        } else if (o.dependencies instanceof Map && o.dependencies.size > 0) {
+          return new LSRichOperation(o.id, o.clock, logootSDel, o.dependencies)
+        } else {
+          return new LSRichOperation(o.id, o.clock, logootSDel, new Map())
+        }
+      }
+    }
+    return null
+  }
+  readonly logootSOp: LogootSOperation | undefined
+}
+
 export class LSRichOperation extends RichOperation<LogootSOperation> {
   static fromPlain(o: SafeAny<LSRichOperation>): LSRichOperation | null {
     if (
@@ -28,7 +59,7 @@ export class LSRichOperation extends RichOperation<LogootSOperation> {
         }
       }
     }
-    return null
+    return LSRichOperationV1.fromPlain(o as SafeAny<LSRichOperationV1>)
   }
 
   equals(aOther: LSRichOperation): boolean {
