01a7b12d75428c2a9022c1dea1dec5d214626977
Cédric Enclos <cedric.enclos@loria.fr>
2019-05-13 10:59:07 +0200

    feat(logs): add loclaOperation in the experiment logs

--- src/core/Document.ts
+++ src/core/Document.ts
@@ -71,6 +71,7 @@ export abstract class Document<Seq, Op> extends Disposable {
         const t3 = process.hrtime()
         this.experimentLogsSubject.next({
           type: 'local',
+          localOperation: ope,
           operation: remoteOp,
           time3: t3,
           time4: t4,
@@ -106,6 +107,7 @@ export abstract class Document<Seq, Op> extends Disposable {
           const t4 = process.hrtime()
           this.experimentLogsSubject.next({
             type: 'remote',
+            localOperation: res,
             operation,
             time3: t3,
             time4: t4,
