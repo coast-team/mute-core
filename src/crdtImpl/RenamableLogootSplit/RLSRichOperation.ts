92fcbe857c5e4270dd11b29160a35167fad29d00
Matthieu Nicolas <nicolasmatthieu57@gmail.com>
2019-06-20 08:42:08 +0200

    feat(renamablelogootsplit): add RLSRichOperation

---
+++ src/crdtImpl/RenamableLogootSplit/RLSRichOperation.ts
@@ -0,0 +1,49 @@
+import {
+  LogootSRename,
+  RenamableListOperation,
+  RenamableLogootSAdd,
+  RenamableLogootSDel,
+} from 'mute-structs'
+import { SafeAny } from 'safe-any'
+import { RichOperation } from '../../core'
+
+function isDependencyAsArray(o: unknown): o is [number, number] {
+  return Array.isArray(o) && o.length === 2 && typeof o[0] === 'number' && typeof o[1] === 'number'
+}
+
+function isDependenciesAsArray(o: unknown): o is Array<[number, number]> {
+  return Array.isArray(o) && o.every(isDependencyAsArray)
+}
+
+export class RLSRichOperation extends RichOperation<RenamableListOperation> {
+  static fromPlain(o: SafeAny<RLSRichOperation>): RLSRichOperation | null {
+    if (
+      typeof o === 'object' &&
+      o !== null &&
+      typeof o.id === 'number' &&
+      Number.isInteger(o.id) &&
+      typeof o.clock === 'number' &&
+      Number.isInteger(o.clock) &&
+      isDependenciesAsArray(o.dependencies)
+    ) {
+      const dependencies = new Map(o.dependencies)
+
+      const renamableLogootSAdd = RenamableLogootSAdd.fromPlain(o.operation)
+      if (renamableLogootSAdd instanceof RenamableLogootSAdd) {
+        return new RLSRichOperation(o.id, o.clock, renamableLogootSAdd, dependencies)
+      }
+
+      const renamableLogootSDel = RenamableLogootSDel.fromPlain(o.operation)
+      if (renamableLogootSDel instanceof RenamableLogootSDel) {
+        return new RLSRichOperation(o.id, o.clock, renamableLogootSDel, dependencies)
+      }
+
+      const logootSRename = LogootSRename.fromPlain(o.operation)
+      if (logootSRename instanceof LogootSRename) {
+        return new RLSRichOperation(o.id, o.clock, logootSRename, dependencies)
+      }
+    }
+
+    return null
+  }
+}
